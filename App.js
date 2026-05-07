c79614a5127be23457275817daddfcf4dfd6122b
harshavardhan81860 <harsha81860@gmail.com>
2026-05-07 16:19:18 +0530

    Update App.js

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const handleSend = async () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/chat", {
+      const response = await fetch("/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
